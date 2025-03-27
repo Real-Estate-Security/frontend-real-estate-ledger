9c57f84074b699c4babca8164f3bbc33ec25295d
mahikaperi <mahikaperi@tamu.edu>
2025-03-27 17:43:54 -0500

    table made

--- src/pages/Listings.tsx
+++ src/pages/Listings.tsx
@@ -1,5 +1,60 @@
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+// const LISTINGS = [ //example from shacn documentation - replace w listing info from table SQL
+//   {
+//     invoice: "INV001",
+//     paymentStatus: "Paid",
+//     totalAmount: "$250.00",
+//     paymentMethod: "Credit Card",
+//   },
+//   {
+//     invoice: "INV002",
+//     paymentStatus: "Pending",
+//     totalAmount: "$150.00",
+//     paymentMethod: "PayPal",
+//   },
+//   {
+//     invoice: "INV003",
+//     paymentStatus: "Unpaid",
+//     totalAmount: "$350.00",
+//     paymentMethod: "Bank Transfer",
+//   },
+//   {
+//     invoice: "INV004",
+//     paymentStatus: "Paid",
+//     totalAmount: "$450.00",
+//     paymentMethod: "Credit Card",
+//   },
+//   {
+//     invoice: "INV005",
+//     paymentStatus: "Paid",
+//     totalAmount: "$550.00",
+//     paymentMethod: "PayPal",
+//   },
+//   {
+//     invoice: "INV006",
+//     paymentStatus: "Pending",
+//     totalAmount: "$200.00",
+//     paymentMethod: "Bank Transfer",
+//   },
+//   {
+//     invoice: "INV007",
+//     paymentStatus: "Unpaid",
+//     totalAmount: "$300.00",
+//     paymentMethod: "Credit Card",
+//   },
+// ]
 
 const ViewListings = () => {
   const navigate = useNavigate();
@@ -32,9 +87,43 @@ const ViewListings = () => {
             </div>
          </div>
         </div>
+
+  <Table>
+  <TableCaption>A list of all current listings.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Agent First Name</TableHead>
+      <TableHead>Address</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Owner First Name</TableHead>
+      <TableHead>Owner Last Name</TableHead>
+      <TableHead className="text-right">Price</TableHead>
+      <TableHead>Bedrooms</TableHead>
+      <TableHead>Bathrooms</TableHead>
+      <TableHead>Details</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    {/* {invoices.map((invoice) => (
+      <TableRow key={invoice.invoice}>
+        <TableCell className="font-medium">{invoice.invoice}</TableCell>
+        <TableCell>{invoice.paymentStatus}</TableCell>
+        <TableCell>{invoice.paymentMethod}</TableCell>
+        <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+      </TableRow>
+    ))} */}
+  </TableBody>
+  <TableFooter>
+    <TableRow>
+      <TableCell colSpan={3}>Total</TableCell>
+      <TableCell className="text-right">$2,500.00</TableCell>
+    </TableRow>
+  </TableFooter>
+  </Table>
       </section>
     </div>
-  )
+)
+  
 }
 
 export default ViewListings;
