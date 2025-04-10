51f5d0a1b574e8374d964b9c1414d26ab97317ba
mahikaperi <mahikaperi@tamu.edu>
2025-04-10 12:01:38 -0500

    complete listingDisplayService

--- src/services/listingDisplayService.ts
+++ src/services/listingDisplayService.ts
@@ -1,22 +1,16 @@
-import axios from "../utils/axiosInstance";
+ import axios from "../utils/axiosInstance";
 import { components } from "../api/types/api-types";
 
 const API_BASE_URL = "http://localhost:8000";
 
-export type ListingDisplayResponse = components["schemas"]["server.listingDisplayResponse"];
+export type ListingDisplayResponse =
+  components["schemas"]["server.listingDisplayResponse"];
 
 // Display Listing function
-export const listing = async (): Promise<ListingDisplayResponse> => {
-    const response = await axios.get<ListingDisplayResponse[](
-      `${API_BASE_URL}/listing`
-    );
-    return response.data;
-  };
-
-
-//   export const getRepresentations = async (): Promise<Representation[]> => {
-//     const response = await axios.get<Representation[]>(
-//       `${API_BASE_URL}/agent/representation`
-//     );
-//     return response.data;
-//   };
+export const listing = async (
+): Promise<ListingDisplayResponse[]> => {
+  const response = await axios.get<ListingDisplayResponse[]>(
+    `${API_BASE_URL}/listing`
+  );
+  return response.data;
+};
