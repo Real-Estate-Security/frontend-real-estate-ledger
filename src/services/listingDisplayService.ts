db50dfc3a9cc7799368a22993d5fc7d7aae95015
briancoco <briannguyencoco@gmail.com>
2025-04-15 11:40:56 -0500

    Refactor styling

--- src/services/listingDisplayService.ts
+++ src/services/listingDisplayService.ts
@@ -1,14 +1,16 @@
- import axios from "../utils/axiosInstance";
+import axios from "../utils/axiosInstance";
 import { components } from "../api/types/api-types";
 
 const API_BASE_URL = "http://localhost:8000";
 
 export type ListingDisplayResponse =
-  components["schemas"]["server.listingDisplayResponse"];
+  components["schemas"]["server.listingDisplayResponse"] & {
+    id: number;
+    property_id: number;
+  };
 
 // Display Listing function
-export const getListings = async (
-): Promise<ListingDisplayResponse[]> => {
+export const getListings = async (): Promise<ListingDisplayResponse[]> => {
   const response = await axios.get<ListingDisplayResponse[]>(
     `${API_BASE_URL}/listing`
   );
