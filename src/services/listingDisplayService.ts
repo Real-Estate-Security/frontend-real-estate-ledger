aeb7541b25f366247d3e8a8d574816cbd67966f3
briancoco <briannguyencoco@gmail.com>
2025-04-15 11:45:30 -0500

    revert data types

--- src/services/listingDisplayService.ts
+++ src/services/listingDisplayService.ts
@@ -4,10 +4,7 @@ import { components } from "../api/types/api-types";
 const API_BASE_URL = "http://localhost:8000";
 
 export type ListingDisplayResponse =
-  components["schemas"]["server.listingDisplayResponse"] & {
-    id: number;
-    property_id: number;
-  };
+  components["schemas"]["server.listingDisplayResponse"];
 
 // Display Listing function
 export const getListings = async (): Promise<ListingDisplayResponse[]> => {
